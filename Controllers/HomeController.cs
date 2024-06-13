667cbf43b069cf576da4dec98ae8d1e7da50ee64
Mustapha Onjoko <onjo0002@algonquinlive.com>
2024-06-13 16:23:28 -0400

    addditional commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab4.Models;
+
+namespace Lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SongForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sing(int numberOfMonkeys)
+        {
+            ViewBag.NumberOfMonkeys = numberOfMonkeys;
+            return View();
+        }
+
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student)
+        {
+            return View(student);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
